7dbb2039d2f22ac99912d9ff37bf07086e44ad84
Michi1090 <michi.m1090@gmail.com>
2022-03-09 19:12:44 +0900

    検索機能をGETメソッドに変更、検索とページネーションの両立実装

--- logs/index.php
+++ logs/index.php
@@ -21,93 +21,87 @@ if (isset($_SESSION['id'])) {
 // セレクトボックスの値
 require_once('select_box.php');
 
-if ($_SERVER['REQUEST_METHOD'] == 'GET') { // GETでアクセスした場合
-    // 入力フォームの初期値
-    $date = $part = $machine = '';
+// 検索条件の値を代入
+$date = filter_input(INPUT_GET, 'date');
+$part = filter_input(INPUT_GET, 'part');
+$machine = filter_input(INPUT_GET, 'machine');
 
-    //SQL文を変数にいれる。$count_sqlはデータの件数取得に使うための変数。
-    $count_sql = 'SELECT COUNT(*) as cnt FROM weight_logs  WHERE user_id = :user_id ';
+// 検索条件をクエリーパラメータに格納
+$search_conditions = '&date=' . $date . '&part=' . $part . '&machine=' . $machine;
 
-    //最大ページ数を取得する。
-    //５件ずつ表示させているので、$count['cnt']に入っている件数を５で割って小数点は切りあげると最大ページ数になる。
-    $counts = $pdo->prepare($count_sql);
-    $counts->bindValue(':user_id', $user_id, PDO::PARAM_INT);
-    $counts->execute();
-    $count = $counts->fetch(PDO::FETCH_ASSOC);
-    $max_page = ceil($count['cnt'] / 5);
-
-    //ページ数を取得する。GETでページが渡ってこなかった時(最初のページ)のときは$pageに１を格納する。
-    if (isset($_GET['page']) && is_numeric($_GET['page'])) {
-        $page = $_GET['page'];
-    } else {
-        $page = 1;
-    }
-
-    // ページ番号
-    if ($page == 1 || $page == $max_page) {
-        $range = 4;
-    } elseif ($page == 2 || $page == $max_page - 1) {
-        $range = 3;
-    } else {
-        $range = 2;
-    }
-
-    // ?件目の表示
-    $from_record = ($page - 1) * 5 + 1;
-
-    if ($page == $max_page && $count['cnt'] % 5 !== 0
-    ) {
-        $to_record = ($page - 1) * 5 + $count['cnt'] % 5;
-    } else {
-        $to_record = $page * 5;
-    }
-
-    // ユーザーの全筋トレログを取得
-    $sql = 'SELECT weight_logs.id, date, part, machine, weight, time, set_count, work_load, note FROM weight_logs JOIN users ON user_id = users.id WHERE user_id = :user_id ORDER BY date DESC LIMIT :start, 5';
-    $stmt = $pdo->prepare($sql);
-    $stmt->bindValue(':user_id', $user_id, PDO::PARAM_INT);
-    $stmt->bindValue(':start', $from_record - 1, PDO::PARAM_INT);
-    $stmt->execute();
-    $result = $stmt->fetchAll();
-} elseif ($_SERVER['REQUEST_METHOD'] == 'POST') { // フォームから値が入力された場合
-    // フォームの入力値を代入
-    $date = $_POST['date'];
-    $part = $_POST['part'];
-    $machine = $_POST['machine'];
-
-    // 条件（where句）の生成
-    $where = "";
-    if (!empty($date)) {
-        $where .= ' AND date = :date';
-    }
-
-    if (!empty($part)) {
-        $where .= ' AND part = :part';
-    }
-
-    if (!empty($machine)) {
-        $where .= ' AND machine LIKE :machine';
-    }
-
-    // 条件に合致するレコードを取得
-    $sql = 'SELECT weight_logs.id, date, part, machine, weight, time, set_count, work_load, note FROM weight_logs JOIN users ON user_id = users.id WHERE user_id = :user_id' . $where . ' ORDER BY date DESC';
-    $stmt = $pdo->prepare($sql);
-    $stmt->bindValue(':user_id', $user_id, PDO::PARAM_INT);
-    if (!empty($date)) {
-        $stmt->bindValue(':date', $date, PDO::PARAM_STR);
-    }
-
-    if (!empty($part)) {
-        $stmt->bindValue(':part', $part, PDO::PARAM_STR);
-    }
-
-    if (!empty($machine)) {
-        $stmt->bindValue(':machine', '%' . $machine . '%', PDO::PARAM_STR);
-    }
-
-    $stmt->execute();
-    $result = $stmt->fetchAll();
+// 条件（where句）の生成
+$where = "";
+if (!empty($date)) {
+    $where .= ' AND date = :date';
 }
+
+if (!empty($part)) {
+    $where .= ' AND part = :part';
+}
+
+if (!empty($machine)) {
+    $where .= ' AND machine LIKE :machine';
+}
+
+//最大ページ数の取得
+$count_sql = 'SELECT COUNT(*) as cnt FROM weight_logs  WHERE user_id = :user_id' . $where;
+$counts = $pdo->prepare($count_sql);
+$counts->bindValue(':user_id', $user_id, PDO::PARAM_INT);
+if (!empty($date)) {
+    $counts->bindValue(':date', $date, PDO::PARAM_STR);
+}
+
+if (!empty($part)) {
+    $counts->bindValue(':part', $part, PDO::PARAM_STR);
+}
+
+if (!empty($machine)) {
+    $counts->bindValue(':machine', '%' . $machine . '%', PDO::PARAM_STR);
+}
+$counts->execute();
+$count = $counts->fetch(PDO::FETCH_ASSOC);
+$max_page = (int)ceil($count['cnt'] / 5);
+
+//ページ数を取得する。GETでページが渡ってこなかった時(最初のページ)のときは$pageに１を格納する
+if (isset($_GET['page']) && is_numeric($_GET['page'])) {
+    $page = (int)$_GET['page'];
+} else {
+    $page = 1;
+}
+
+// ページ番号
+if ($page === 1 || $page === $max_page) {
+    $range = 4;
+} elseif ($page === 2 || $page === $max_page - 1) {
+    $range = 3;
+} else {
+    $range = 2;
+}
+
+// ?件目の表示
+$from_record = ($page - 1) * 5 + 1;
+if ($page === $max_page && $count['cnt'] % 5 !== 0) {
+    $to_record = ($page - 1) * 5 + $count['cnt'] % 5;
+} else {
+    $to_record = $page * 5;
+}
+
+// ページと検索条件に合致するトレーニングログを5件取得
+$sql = 'SELECT weight_logs.id, date, part, machine, weight, time, set_count, work_load, note FROM weight_logs JOIN users ON user_id = users.id WHERE user_id = :user_id' . $where . ' ORDER BY date DESC LIMIT :from, 5';
+$stmt = $pdo->prepare($sql);
+$stmt->bindValue(':user_id', $user_id, PDO::PARAM_INT);
+if (!empty($date)) {
+    $stmt->bindValue(':date', $date, PDO::PARAM_STR);
+}
+if (!empty($part)) {
+    $stmt->bindValue(':part', $part, PDO::PARAM_STR);
+}
+if (!empty($machine)) {
+    $stmt->bindValue(':machine', '%' . $machine . '%', PDO::PARAM_STR);
+}
+$stmt->bindValue(':from', $from_record - 1, PDO::PARAM_INT);
+$stmt->execute();
+$result = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 // ヘッダーのパス指定
 $path_logs = './';
@@ -124,10 +118,10 @@ $path_users = '../users/';
     <!-- header 読み込み -->
     <?php require_once('../header.php') ?>
 
-    <h2><?= escape($message); ?></h2>
+    <h2><?= escape($message) ?></h2>
 
     <!-- 検索フォーム -->
-    <form method="post">
+    <form method="get">
         <div>
             <label for="date">日付</label>
             <input type="date" name="date" max="9999-12-31" id="date" value="<?= escape($date) ?>">
@@ -189,29 +183,29 @@ $path_users = '../users/';
     <!-- 戻るボタン -->
     <div class="pagination">
         <?php if ($page >= 2) : ?>
-            <a href="index.php?page=<?php echo ($page - 1); ?>" class="page_feed">&laquo;</a>
-        <?php else :; ?>
+            <a href="index.php?page=<?= escape(($page - 1) . $search_conditions) ?>" class="page_feed">&laquo;</a>
+        <?php else : ?>
             <span class="first_last_page">&laquo;</span>
-        <?php endif; ?>
+        <?php endif ?>
         <!-- ページ番号 -->
         <?php for ($i = 1; $i <= $max_page; $i++) : ?>
             <?php if ($i >= $page - $range && $i <= $page + $range) : ?>
-                <?php if ($i == $page) : ?>
-                    <span class="now_page_number"><?php echo $i; ?></span>
+                <?php if ($i === $page) : ?>
+                    <span class="now_page_number"><?= escape($i) ?></span>
                 <?php else : ?>
-                    <a href="?page=<?php echo $i; ?>" class="page_number"><?php echo $i; ?></a>
-                <?php endif; ?>
-            <?php endif; ?>
-        <?php endfor; ?>
+                    <a href="?page=<?= escape($i . $search_conditions) ?>" class="page_number"><?= escape($i) ?></a>
+                <?php endif ?>
+            <?php endif ?>
+        <?php endfor ?>
         <!-- 進む -->
         <?php if ($page < $max_page) : ?>
-            <a href="index.php?page=<?php echo ($page + 1); ?>" class="page_feed">&raquo;</a>
+            <a href="index.php?page=<?= escape(($page + 1) . $search_conditions) ?>" class="page_feed">&raquo;</a>
         <?php else : ?>
             <span class="first_last_page">&raquo;</span>
         <?php endif; ?>
     </div>
     <!-- ?件目の表示 -->
-    <p class="from_to"><?php echo $count['cnt']; ?>件中 <?php echo $from_record; ?> - <?php echo $to_record; ?> 件目を表示</p>
+    <p class="from_to"><?= escape($count['cnt'] . '件中' . $from_record . '-' . $to_record . '件目を表示') ?></p>
 
     <script src="../js/bootstrap.bundle.min.js"></script>
 </body>
