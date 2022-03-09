ae4e1833fe93d8671d29802e5f5058b8b00d39d9
Michi1090 <michi.m1090@gmail.com>
2022-03-09 23:18:55 +0900

    ページネーションにBootstrap実装

--- logs/index.php
+++ logs/index.php
@@ -118,94 +118,109 @@ $path_users = '../users/';
     <!-- header 読み込み -->
     <?php require_once('../header.php') ?>
 
-    <h2><?= escape($message) ?></h2>
+    <main>
+        <h2><?= escape($message) ?></h2>
 
-    <!-- 検索フォーム -->
-    <form method="get">
-        <div>
-            <label for="date">日付</label>
-            <input type="date" name="date" max="9999-12-31" id="date" value="<?= escape($date) ?>">
-        </div>
-        <div>
-            <label for="part">部位</label>
-            <select name="part" id="part">
-                <option value="">--</option>
-                <?php foreach ($form_parts as $form_part) : ?>
-                    <!-- 検索で入力された値とする場合、selected属性を付加する -->
-                    <option <?= $form_part === $part ? 'selected' : '' ?>><?= escape($form_part) ?></option>
-                <?php endforeach ?>
-            </select>
-        </div>
-        <div>
-            <label for="machine">マシン</label>
-            <input type="text" name="machine" value="<?= escape($machine) ?>">
-        </div>
-        <input type="submit" value="検索">
-        <input type="button" value="クリア" onclick="location.href='index.php'">
-    </form>
+        <!-- 検索フォーム -->
+        <form method="get">
+            <div>
+                <label for="date">日付</label>
+                <input type="date" name="date" max="9999-12-31" id="date" value="<?= escape($date) ?>">
+            </div>
+            <div>
+                <label for="part">部位</label>
+                <select name="part" id="part">
+                    <option value="">--</option>
+                    <?php foreach ($form_parts as $form_part) : ?>
+                        <!-- 検索で入力された値とする場合、selected属性を付加する -->
+                        <option <?= $form_part === $part ? 'selected' : '' ?>><?= escape($form_part) ?></option>
+                    <?php endforeach ?>
+                </select>
+            </div>
+            <div>
+                <label for="machine">マシン</label>
+                <input type="text" name="machine" value="<?= escape($machine) ?>">
+            </div>
+            <input type="submit" value="検索">
+            <input type="button" value="クリア" onclick="location.href='index.php'">
+        </form>
 
-    <!-- 筋トレログ表示 -->
-    <div>
-        <?php if (!empty($result)) : ?>
-            <table>
-                <tr>
-                    <th>日付</th>
-                    <th>部位</th>
-                    <th>マシン</th>
-                    <th>重量</th>
-                    <th>回数</th>
-                    <th>セット</th>
-                    <th>負荷</th>
-                    <th>メモ</th>
-                </tr>
-                <?php foreach ($result as $log) : ?>
+        <!-- 筋トレログ表示 -->
+        <div>
+            <?php if (!empty($result)) : ?>
+                <table>
                     <tr>
-                        <?php $id = $log['id']; ?>
-                        <td><?= escape($log['date']) ?></td>
-                        <td><?= escape($log['part']) ?></td>
-                        <td><?= escape($log['machine']) ?></td>
-                        <td><?= escape($log['weight']) . ' kg' ?></td>
-                        <td><?= escape($log['time']) . ' 回' ?></td>
-                        <td><?= escape($log['set_count']) . ' set' ?></td>
-                        <td><?= escape($log['work_load']) ?></td>
-                        <td> <?= escape($log['note']) ?></td>
-                        <td><a href="edit.php?id=<?= $id ?>">編集</a></td>
-                        <td><a href="delete.php?id=<?= $id ?>">削除</a></td>
+                        <th>日付</th>
+                        <th>部位</th>
+                        <th>マシン</th>
+                        <th>重量</th>
+                        <th>回数</th>
+                        <th>セット</th>
+                        <th>負荷</th>
+                        <th>メモ</th>
                     </tr>
-                <?php endforeach ?>
-            </table>
-        <?php else : ?>
-            <p>該当する筋トレログはありません</p>
-        <?php endif ?>
-    </div>
-
-    <!-- ページネーション -->
-    <!-- 戻るボタン -->
-    <div class="pagination">
-        <?php if ($page >= 2) : ?>
-            <a href="index.php?page=<?= escape(($page - 1) . $search_conditions) ?>" class="page_feed">&laquo;</a>
-        <?php else : ?>
-            <span class="first_last_page">&laquo;</span>
-        <?php endif ?>
-        <!-- ページ番号 -->
-        <?php for ($i = 1; $i <= $max_page; $i++) : ?>
-            <?php if ($i >= $page - $range && $i <= $page + $range) : ?>
-                <?php if ($i === $page) : ?>
-                    <span class="now_page_number"><?= escape($i) ?></span>
-                <?php else : ?>
-                    <a href="?page=<?= escape($i . $search_conditions) ?>" class="page_number"><?= escape($i) ?></a>
-                <?php endif ?>
+                    <?php foreach ($result as $log) : ?>
+                        <tr>
+                            <?php $id = $log['id']; ?>
+                            <td><?= escape($log['date']) ?></td>
+                            <td><?= escape($log['part']) ?></td>
+                            <td><?= escape($log['machine']) ?></td>
+                            <td><?= escape($log['weight']) . ' kg' ?></td>
+                            <td><?= escape($log['time']) . ' 回' ?></td>
+                            <td><?= escape($log['set_count']) . ' set' ?></td>
+                            <td><?= escape($log['work_load']) ?></td>
+                            <td> <?= escape($log['note']) ?></td>
+                            <td><a href="edit.php?id=<?= $id ?>">編集</a></td>
+                            <td><a href="delete.php?id=<?= $id ?>">削除</a></td>
+                        </tr>
+                    <?php endforeach ?>
+                </table>
+            <?php else : ?>
+                <p>該当する筋トレログはありません</p>
             <?php endif ?>
-        <?php endfor ?>
-        <!-- 進む -->
-        <?php if ($page < $max_page) : ?>
-            <a href="index.php?page=<?= escape(($page + 1) . $search_conditions) ?>" class="page_feed">&raquo;</a>
-        <?php else : ?>
-            <span class="first_last_page">&raquo;</span>
-        <?php endif; ?>
-    </div>
-    <!-- ?件目の表示 -->
-    <p class="from_to"><?= escape($count['cnt'] . '件中' . $from_record . '-' . $to_record . '件目を表示') ?></p>
+        </div>
+
+        <!-- ページネーション -->
+        <nav>
+            <!-- 戻るボタン -->
+            <ul class="pagination justify-content-center">
+                <?php if ($page >= 2) : ?>
+                    <li class="page-item">
+                        <a class="page-link" href="index.php?page=1<?= escape($search_conditions) ?>">&laquo;</a>
+                    </li>
+                    <li class="page-item">
+                        <a class="page-link" href="index.php?page=<?= escape(($page - 1) . $search_conditions) ?>">&lt; 前ページ</a>
+                    </li>
+                <?php else : ?>
+                    <li class="page-item">
+                        <span class="page-link">&laquo;</span>
+                    </li>
+                    <li class="page-item">
+                        <span class="page-link">&lt; 前ページ</span>
+                    </li>
+                <?php endif ?>
+                <!-- 進む -->
+                <?php if ($page < $max_page) : ?>
+                    <li class="page-item">
+                        <a class="page-link" href="index.php?page=<?= escape(($page + 1) . $search_conditions) ?>">次ページ &gt;</a>
+                    </li>
+                    <li class="page-item">
+                        <a class="page-link" href="index.php?page=<?= escape($max_page . $search_conditions) ?>">&raquo;</a>
+                    </li>
+                <?php else : ?>
+                    <li class="page-item">
+                        <span class="page-link">次ページ &gt;</span>
+                    </li>
+                    <li class="page-item">
+                        <span class="page-link">&raquo;</span>
+                    </li>
+                <?php endif ?>
+            </ul>
+            <!-- ?件目の表示 -->
+            <p class="text-center"><?= escape($count['cnt'] . '件中' . $from_record . '-' . $to_record . '件目を表示') ?></p>
+        </nav>
+
+    </main>
 
     <script src="../js/bootstrap.bundle.min.js"></script>
 </body>
