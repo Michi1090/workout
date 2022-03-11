85eac2f739dc3f63dd0890dd24148bc044b54eda
Michi1090 <michi.m1090@gmail.com>
2022-03-11 16:59:07 +0900

    ログ編集・削除画面 存在しないログidでGET送信したときの対策追加

--- logs/delete.php
+++ logs/delete.php
@@ -22,6 +22,7 @@ $sql = 'SELECT date, part, machine, weight, time, set_count, work_load, note FRO
 $stmt = $pdo->prepare($sql);
 $stmt->bindValue(':id', $id, PDO::PARAM_INT);
 $stmt->execute();
+$logs = $stmt->fetch(PDO::FETCH_ASSOC);
 
 // フォームから値が入力された場合
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
@@ -57,39 +58,45 @@ $path_users = '../users/';
     <main>
         <div class="container">
             <div class="justify-content-center">
-                <!-- カード -->
-                <div class="card">
-                    <!-- カードヘッダー -->
-                    <div class="card-header">
-                        <h1 class="text-center my-2">トレーニングログ削除</h1>
-                    </div>
-                    <!-- カードボディ -->
-                    <div class="card-body">
-                        <p class="mb-2">このトレーニングログを削除します。よろしいですか？</p>
-                        <!-- 削除内容 -->
-                        <div class="border-top border-bottom py-2">
-                            <?php foreach ($stmt as $log) : ?>
-                                <p class="mb-2">日付 : <?= escape($log['date']) ?></p>
-                                <p class="mb-2">部位 : <?= escape($log['part']) ?></p>
-                                <p class="mb-2">マシン : <?= escape($log['machine']) ?></p>
-                                <p class="mb-2">重量 : <?= escape($log['weight']) ?> kg</p>
-                                <p class="mb-2">回数 : <?= escape($log['time']) ?> 回</p>
-                                <p class="mb-2">セット : <?= escape($log['set_count']) ?> set</p>
-                                <p class="mb-2">負荷 : <?= escape($log['work_load']) ?></p>
-                                <p class="mb-0">メモ : <?= escape($log['note']) ?></p>
-                            <?php endforeach ?>
-                        </div>
 
-                        <!-- 削除フォーム -->
-                        <form class="mt-3" method="post">
-                            <input type="hidden" name="id" value="<?= escape($id) ?>">
-                            <div class="d-grid gap-3">
-                                <a class="btn btn-secondary" href="index.php">戻る</a>
-                                <button class="btn btn-danger" type="submit">削除</button>
+                <!-- 対象のログidが存在する場合  -->
+                <?php if (!empty($logs)) : ?>
+                    <!-- カード -->
+                    <div class="card">
+                        <!-- カードヘッダー -->
+                        <div class="card-header">
+                            <h1 class="text-center my-2">トレーニングログ削除</h1>
+                        </div>
+                        <!-- カードボディ -->
+                        <div class="card-body">
+                            <p class="mb-2">このトレーニングログを削除します。よろしいですか？</p>
+                            <!-- 削除内容 -->
+                            <div class="border-top border-bottom py-2">
+                                <p class="mb-2">日付 : <?= escape($logs['date']) ?></p>
+                                <p class="mb-2">部位 : <?= escape($logs['part']) ?></p>
+                                <p class="mb-2">マシン : <?= escape($logs['machine']) ?></p>
+                                <p class="mb-2">重量 : <?= escape($logs['weight']) ?> kg</p>
+                                <p class="mb-2">回数 : <?= escape($logs['time']) ?> 回</p>
+                                <p class="mb-2">セット : <?= escape($logs['set_count']) ?> set</p>
+                                <p class="mb-2">負荷 : <?= escape($logs['work_load']) ?></p>
+                                <p class="mb-0">メモ : <?= escape($logs['note']) ?></p>
                             </div>
-                        </form>
+
+                            <!-- 削除フォーム -->
+                            <form class="mt-3" method="post">
+                                <input type="hidden" name="id" value="<?= escape($id) ?>">
+                                <div class="d-grid gap-3">
+                                    <a class="btn btn-secondary" href="index.php">戻る</a>
+                                    <button class="btn btn-danger" type="submit">削除</button>
+                                </div>
+                            </form>
+                        </div>
                     </div>
-                </div>
+
+                    <!-- 該当ログなしの場合  -->
+                <?php else : ?>
+                    <h6>※該当するトレーニングログが存在しません</h6>
+                <?php endif ?>
             </div>
         </div>
     </main>
