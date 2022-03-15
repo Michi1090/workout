0746fe8ef27848366b1a1e6e9366744254c8d6ec
Michi1090 <michi.m1090@gmail.com>
2022-03-15 20:40:56 +0900

    DBの環境変数設定

--- src/common/db_connect.php
+++ src/common/db_connect.php
@@ -3,10 +3,16 @@
 // データベース接続
 try {
     // データベースに接続してPDOインスタンスを作成
-    $dsn = 'mysql:host=localhost;dbname=workout;charset=utf8mb4';
-    $user = 'root';
-    $pass = '';
+    $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+    $db_name = substr($url["path"], 1);
+    $db_host = $url["host"];
+    $dsn = "mysql:dbname=" . $db_name . ";host=" . $db_host;
+
+    $user = $url["user"];
+    $pass = $url["pass"];
     $driver_options = array(PDO::ATTR_PERSISTENT => true);
+    
     $pdo = new PDO($dsn, $user, $pass, $driver_options);
 } catch (PDOException $e) {
     // DBアクセスに失敗した場合、エラーメッセージを表示
