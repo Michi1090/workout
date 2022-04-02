a828b8ea2f1c8b18fa921ba2f1de40cc6ea21416
Michi1090 <michi.m1090@gmail.com>
2022-04-02 12:57:15 +0900

    ファビコンの相対パス修正

--- src/common/head.php
+++ src/common/head.php
@@ -9,11 +9,11 @@
 
     <!-- ファビコン
     =============================================================================================================================== -->
-    <link rel="apple-touch-icon" sizes="180x180" href="../image/favicon/apple-touch-icon.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="../image/favicon/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="../image/favicon/favicon-16x16.png">
-    <link rel="manifest" href="../image/favicon/site.webmanifest">
-    <link rel="mask-icon" href="../image/favicon/safari-pinned-tab.svg" color="#ffc107">
+    <link rel="apple-touch-icon" sizes="180x180" href="<?= escape($path['root']) ?>image/favicon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="<?= escape($path['root']) ?>image/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="<?= escape($path['root']) ?>image/favicon/favicon-16x16.png">
+    <link rel="manifest" href="<?= escape($path['root']) ?>image/favicon/site.webmanifest">
+    <link rel="mask-icon" href="<?= escape($path['root']) ?>image/favicon/safari-pinned-tab.svg" color="#ffc107">
     <meta name="msapplication-TileColor" content="#ffc107">
     <meta name="theme-color" content="#ffc107">
 
